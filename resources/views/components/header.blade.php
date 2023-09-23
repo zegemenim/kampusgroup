431df4ac8d058f7845daa5e31119093aa793e298
Zahit Egemen İm <zegemenim@gmail.com>
2023-09-23 16:10:55 +0300

    Improved

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="tr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Kampüs Group</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body class="bg-black">
+<header class=" border-b-2 border-yellow-400">
+    <div class="m-4">
+        <img class="w-64 " src="{{asset("images/logo.jpg")}}" alt="">
+    </div>
+</header>
+
