3d04b6db3f08c2e0941913636c5ad73fe1e81e44
Zahit Egemen İm <zegemenim@gmail.com>
2023-09-23 17:24:36 +0300

    Update header.blade.php

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,3 +1,4 @@
+
 <!doctype html>
 <html lang="tr">
 <head>
@@ -7,10 +8,21 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Kampüs Group</title>
     <script src="https://cdn.tailwindcss.com"></script>
+
 </head>
-<body class="bg-black">
-<header class=" border-b-2 border-yellow-400">
-    <div class="m-4">
+<body class="bg-black ">
+<header class="grid grid-cols-3 border-b-2 border-yellow-400">
+    <div class="m-4 grid">
+        <a href="{{route("home")}}">
+        <img class="w-64 " src="{{asset("images/logo.jpg")}}" alt="">
+        </a>
+    </div>
+    <div class="m-4 grid text-center justify-center text-white">
+        <a href="tel:+905358904888">
+            535 890 48 88
+        </a>
+    </div>
+    <div class="m-4 grid justify-end">
         <img class="w-64 " src="{{asset("images/logo.jpg")}}" alt="">
     </div>
 </header>
