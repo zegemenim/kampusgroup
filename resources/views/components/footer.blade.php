8bdb04d7bca760752e7818bcbe9c19b8b2e3accf
faramps <yasin.dikdere123@gmail.com>
2023-09-24 14:46:04 +0300

    improved footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,2 +1,91 @@
+<footer class="grid md:grid-cols-3 border-t-2 border-yellow-400 justify-center text-center items-center justify-items-center">
+    <div class="flex text-yellow-400 text-2xl justify-center items-center text-center">
+        <ul>
+            <li class="text-3xl m-6 font-bold">İletişim</li>
+            <li class=" text-2xl">Telefon: 0535 890 48 88</li>
+            <li class="m-4 text-2xl">E-posta: serkan.yilmaz.ds@hotmail.com.tr</li>
+        </ul>
+    </div>
+    <div class="flex text-yellow-400 text-2xl justify-center items-center text-center ">
+        <ul class="text-3xl m-6 font-bold">Bizi takip edin
+
+
+            <div class="grid gap-3 grid-cols-3 justify-center items-center text-center m-6">
+
+                <li class=" justify-center items-center text-center flex ">
+                    <a href="https://kampusgayrimenkul.sahibinden.com/" class="flex">
+                        <svg fill="#FBBF24" class="rounded-full  " width="32px" height="32px" viewBox="0 0 24.00 24.00"
+                             role="img"
+                             xmlns="http://www.w3.org/2000/svg" stroke="#FBBF24" stroke-width="0.00024000000000000003">
+                            <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                            <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"
+                               stroke="#2c2a2a"
+                               stroke-width="4.8"><title>Sahibinden</title>
+                                <path
+                                    d="M0 0v24h24V0zm11.517 4.723c.563-.007 1.13-.004 1.69.063 2.412.054 4.853 2.18 4.879 4.508h-3.319c.009-.694-.603-1.555-1.279-1.732-1.105-.269-2.46-.355-3.43.294-.738.445-1.065 1.672-.095 2.056 2.288 1.083 5.158.846 7.224 2.372 1.698 1.21 1.598 3.666.274 5.086-1.718 1.84-4.636 2.132-7.099 1.782-2.448-.117-4.755-2.245-4.819-4.562h3.311c-.056.832.638 1.557 1.46 1.822 1.27.275 2.726.358 3.93-.19.96-.323 1.024-1.544.284-2.103-1.595-.897-3.565-.924-5.297-1.518-2.012-.39-3.643-2.278-3.26-4.197.424-2.342 3.127-3.727 5.546-3.681z"></path>
+                            </g>
+                            <g id="SVGRepo_iconCarrier"><title>Sahibinden</title>
+                                <path
+                                    d="M0 0v24h24V0zm11.517 4.723c.563-.007 1.13-.004 1.69.063 2.412.054 4.853 2.18 4.879 4.508h-3.319c.009-.694-.603-1.555-1.279-1.732-1.105-.269-2.46-.355-3.43.294-.738.445-1.065 1.672-.095 2.056 2.288 1.083 5.158.846 7.224 2.372 1.698 1.21 1.598 3.666.274 5.086-1.718 1.84-4.636 2.132-7.099 1.782-2.448-.117-4.755-2.245-4.819-4.562h3.311c-.056.832.638 1.557 1.46 1.822 1.27.275 2.726.358 3.93-.19.96-.323 1.024-1.544.284-2.103-1.595-.897-3.565-.924-5.297-1.518-2.012-.39-3.643-2.278-3.26-4.197.424-2.342 3.127-3.727 5.546-3.681z"></path>
+                            </g>
+                        </svg>
+                    </a>
+                <li class="justify-center items-center text-center flex">
+                    <a href="https://www.facebook.com/@Kamp%C3%BCs-Gayrimenk%C3%BCl-Apart-Kiralama-Merkezi-100063859693636/?paipv=0&eav=AfaXgD4zrue3N1FUQQp9_q6TBLS8bLkQzPf-xRMUf8jB2RTZ725NGl_M1PXEFXZ5A6E&_rdr" class="flex">
+                        <svg fill="#2563EB" height="32px" width="32px" version="1.1" id="Layer_1"
+                             xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+                             viewBox="0 0 310 310" xml:space="preserve" stroke="#2563EB"><g id="SVGRepo_bgCarrier"
+                                                                                            stroke-width="0"></g>
+                            <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                            <g id="SVGRepo_iconCarrier">
+                                <g id="XMLID_834_">
+                                    <path id="XMLID_835_"
+                                          d="M81.703,165.106h33.981V305c0,2.762,2.238,5,5,5h57.616c2.762,0,5-2.238,5-5V165.765h39.064 c2.54,0,4.677-1.906,4.967-4.429l5.933-51.502c0.163-1.417-0.286-2.836-1.234-3.899c-0.949-1.064-2.307-1.673-3.732-1.673h-44.996 V71.978c0-9.732,5.24-14.667,15.576-14.667c1.473,0,29.42,0,29.42,0c2.762,0,5-2.239,5-5V5.037c0-2.762-2.238-5-5-5h-40.545 C187.467,0.023,186.832,0,185.896,0c-7.035,0-31.488,1.381-50.804,19.151c-21.402,19.692-18.427,43.27-17.716,47.358v37.752H81.703 c-2.762,0-5,2.238-5,5v50.844C76.703,162.867,78.941,165.106,81.703,165.106z"></path>
+                                </g>
+                            </g></svg>
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.instagram.com/kampusgayrimenkul/">
+
+                        <svg xmlns="http://www.w3.org/2000/svg" class="" viewBox="0 0 48 48" width="48px" height="48px">
+                            <radialGradient id="yOrnnhliCrdS2gy~4tD8ma" cx="19.38" cy="42.035" r="44.899"
+                                            gradientUnits="userSpaceOnUse">
+                                <stop offset="0" stop-color="#fd5"/>
+                                <stop offset=".328" stop-color="#ff543f"/>
+                                <stop offset=".348" stop-color="#fc5245"/>
+                                <stop offset=".504" stop-color="#e64771"/>
+                                <stop offset=".643" stop-color="#d53e91"/>
+                                <stop offset=".761" stop-color="#cc39a4"/>
+                                <stop offset=".841" stop-color="#c837ab"/>
+                            </radialGradient>
+                            <path fill="url(#yOrnnhliCrdS2gy~4tD8ma)"
+                                  d="M34.017,41.99l-20,0.019c-4.4,0.004-8.003-3.592-8.008-7.992l-0.019-20	c-0.004-4.4,3.592-8.003,7.992-8.008l20-0.019c4.4-0.004,8.003,3.592,8.008,7.992l0.019,20	C42.014,38.383,38.417,41.986,34.017,41.99z"/>
+                            <radialGradient id="yOrnnhliCrdS2gy~4tD8mb" cx="11.786" cy="5.54" r="29.813"
+                                            gradientTransform="matrix(1 0 0 .6663 0 1.849)"
+                                            gradientUnits="userSpaceOnUse">
+                                <stop offset="0" stop-color="#4168c9"/>
+                                <stop offset=".999" stop-color="#4168c9" stop-opacity="0"/>
+                            </radialGradient>
+                            <path fill="url(#yOrnnhliCrdS2gy~4tD8mb)"
+                                  d="M34.017,41.99l-20,0.019c-4.4,0.004-8.003-3.592-8.008-7.992l-0.019-20	c-0.004-4.4,3.592-8.003,7.992-8.008l20-0.019c4.4-0.004,8.003,3.592,8.008,7.992l0.019,20	C42.014,38.383,38.417,41.986,34.017,41.99z"/>
+                            <path fill="#fff"
+                                  d="M24,31c-3.859,0-7-3.14-7-7s3.141-7,7-7s7,3.14,7,7S27.859,31,24,31z M24,19c-2.757,0-5,2.243-5,5	s2.243,5,5,5s5-2.243,5-5S26.757,19,24,19z"/>
+                            <circle cx="31.5" cy="16.5" r="1.5" fill="#fff"/>
+                            <path fill="#fff"
+                                  d="M30,37H18c-3.859,0-7-3.14-7-7V18c0-3.86,3.141-7,7-7h12c3.859,0,7,3.14,7,7v12	C37,33.86,33.859,37,30,37z M18,13c-2.757,0-5,2.243-5,5v12c0,2.757,2.243,5,5,5h12c2.757,0,5-2.243,5-5V18c0-2.757-2.243-5-5-5H18z"/>
+                        </svg>
+                    </a>
+                </li>
+            </div>
+        </ul>
+    </div>
+    <div class="flex m-4 justify-center items-center text-center" style="width: 85%">
+        <iframe class="text-center justify-center items-center" width="100%" height="200" frameborder="0" scrolling="no"
+                marginheight="0" marginwidth="0"
+                src="https://maps.google.com/maps?width=100%25&amp;height=200&amp;hl=en&amp;q=%C3%87amlaralt%C4%B1,%206060.%20Sk.%20No:7,%2020160%20Pamukkale/Denizli+(kamp%C3%BCs%20gayrimenkul)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed">
+            <a href="https://www.maps.ie/population/">Find Population on Map</a></iframe>
+    </div>
+</footer>
 </body>
 </html>
